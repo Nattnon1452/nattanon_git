e6f25a7f1c5e62d9eddb5068529f3da70b1b3d4b
Nattnon1452 <112382007+Nattnon1452@users.noreply.github.com>
2022-09-19 13:35:15 +0700

    เพิ่มไฟล์ index.php เข้าไปใน repository
    เพิ่มไฟล์ index.php เข้าไปใน repository

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<?php include("includes/header.php"); ?>
+</head>
+
+<body>
+<?php include("includes/navbar.php"); ?>
+<div class="container">
+    <!-- Start -->
+<h2>หน้าหลัก</h2>
+<hr>
+welcome
+
+    <!-- End -->
+</div>
+
+</body>
+
+</html>
+<?php include("includes/footer.php"); ?>
